b81efee94fa1bb8b4d00b645c3d18a33d3519398
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-14 14:44:37 +0530

    REFACTOR: pop and poplast methods inlinked list

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -218,8 +218,7 @@ public class LinkedList <Type extends Comparable<Type>> {
 		linkedList.append(70);
 		linkedList.insert(1, 30);
 		System.out.println(linkedList);
-		System.out.println(linkedList.pop());
-		System.out.println("New head element "+linkedList.headItem());
+		System.out.println(linkedList.popLast());
 		System.out.println(linkedList);
 	}
 
