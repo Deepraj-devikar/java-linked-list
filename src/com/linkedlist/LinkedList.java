3a1a6ec046d53520ea0a0d6ab9a7e554c579230b
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-14 14:04:01 +0530

    REFACTOR: create linked list with 56 then append 30 and 70 one after another

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -191,9 +191,9 @@ public class LinkedList <Type extends Comparable<Type>> {
 	public static void main(String[] args) {
 		System.out.println("Welcome to linked list program");
 		LinkedList linkedList = new LinkedList<Integer>();
-		linkedList.add(70);
-		linkedList.add(30);
 		linkedList.add(56);
+		linkedList.append(30);
+		linkedList.append(70);
 		System.out.println(linkedList);
 		
 		
