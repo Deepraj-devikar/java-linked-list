924d919cfd2231829c6888abe39f0dd794eed08b
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-14 13:53:08 +0530

    ADDED: create linked list

--- src/com/linkedlist/Node.java
+++ src/com/linkedlist/Node.java
@@ -1,28 +1,15 @@
 package com.linkedlist;
 
 public class Node <Type extends Comparable<Type>> {
-	private Type item;
-	private Node<Type> next;
+	public Type item;
+	public Node<Type> next;
 	
 	Node(Type item) {
 		super();
 		this.item = item;
 	}
-
-	public Type getItem() {
-		return item;
-	}
-
-	public void setItem(Type item) {
-		this.item = item;
-	}
-
-	public Node<Type> getNext() {
-		return next;
-	}
-
-	public void setNext(Node<Type> next) {
-		this.next = next;
-	}
 	
+	public String toString() {
+		return "{ item => "+item+", next => "+next+" }";
+	}
 }
